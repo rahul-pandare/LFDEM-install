7e7a03516fe2c2a7ffdb368d9f19b16fb31529d9
Rahul Pandare <62763813+rahul-pandare@users.noreply.github.com>
2024-11-05 12:30:04 -0500

    Add files via upload
    LF_DEM config file for Apple M1 processor

---
+++ Makefile_config_Rahul_m1.mk
@@ -0,0 +1,39 @@
+#=======================================#
+#    Parameters to provide              #
+#=======================================#
+
+
+# Use DSFMT instead of MT as a RNG ( yes / no )
+DSFMT_RNG = no
+# Enable use of Metis library ( yes / no )
+UseMetis = yes
+
+# the directory where LF_DEM will be copied on `make install`
+install_dir = /Users/rahul/opt
+
+# C++ compiler
+CXX = g++
+CC = gcc
+
+# Libraries
+#
+# SuiteSparse library install folder
+SUITESPARSE_ROOT = /opt/homebrew/Cellar/suite-sparse/7.8.0
+
+# Extra flags to the compiler, if needed (e.g. optimization flags)
+CXXFLAGS_EXTRA = -I /opt/homebrew/include -DGIT_VERSION="\"42ce875e-dirty\""
+
+
+#======== Linking ==================
+
+# Blas and Lapack (you may want to modify the BLAS, as -lblas might point to non optimized BLAS)
+Blas_Linking_Flags = -lblas
+Lapack_Linking_Flags = -llapack
+# Intel MKL
+# Blas_Linking_Flags = -mkl -lrt
+# Lapack_Linking_Flags =
+
+# Extra linking here, if needed
+Extra_Linking_Flags = /opt/homebrew/lib/libcamd.a /opt/homebrew/lib/libccolamd.a
+# for OpenMP with g++
+# Extra_Linking_Flags = -fopenmp
